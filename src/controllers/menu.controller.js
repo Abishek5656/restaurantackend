401054cd1e955ed086f7e380d7c7bb2f17d7b83f
Abishek Varma <abishek@entrolabs.com>
2024-12-31 15:44:27 +0530

    menu item functionality

---
+++ src/controllers/menu.controller.js
@@ -0,0 +1,86 @@
+import { TryCatch } from "../middleware/error.js";
+import { ErrorHandler } from "../utils/utility.js";
+import { Menu } from "../model/menu.model.js";
+import { Category } from "../model/category.model.js";
+
+export const createItem = TryCatch(async (req, res, next) => {
+  const { name, description, price, image, categoryName } = req.body;
+
+  // Check if the category exists and if the item exists
+  const [existingCategory, existingItem] = await Promise.all([
+    Category.findOne({ categoryName: categoryName?.toLowerCase() }),
+    Menu.findOne({ name: name }),
+  ]);
+
+  let category;
+  if (!existingCategory) {
+    category = await Category.create({
+      categoryName: categoryName,
+    });
+  }
+
+  if (existingItem) return next(new ErrorHandler("Item already exist", 404));
+
+  const createMenu = await Menu.create({
+    name: name,
+    description: description,
+    price: Number(price),
+    categoryName: existingCategory?.categoryName || categoryName,
+    categoryId: existingCategory?._id || category?._id,
+  });
+
+  return res
+    .status(200)
+    .json({ sucess: true, message: "Menu Item Created Successfully" });
+});
+
+export const updateMenuItem = TryCatch(async (req, res, next) => {
+  const { name, description, price, image, categoryName } = req.body;
+
+  const { id } = req.params;
+
+  const [existingCategory, existingMenu] = await Promise.all([
+    Category.findOne({ categoryName: categoryName?.toLowerCase() }),
+    Menu.findById(id),
+  ]);
+
+  let newCategory;
+  if (!existingCategory) {
+    newCategory = await Category.create({
+      categoryName: categoryName?.toLowerCase(),
+    });
+  }
+
+  if (!existingMenu) return next(new ErrorHandler("Menu Item Not Found"));
+
+  const updateMenu = await Menu.findByIdAndUpdate(id, {
+    $set: {
+      name: name,
+      description: description,
+      price: price,
+      categoryName: newCategory?.categoryName || existingCategory?.categoryName,
+      categoryId: newCategory?._id || existingCategory?._id,
+    },
+  });
+
+  return res
+    .status(200)
+    .json({ sucess: true, message: "Menu item updated Sucessfully" });
+});
+
+export const deleteMenuItem = TryCatch(async (req, res, next) => {
+  const { id } = req.params;
+
+  const existMenuItem = await Menu.findById(id);
+
+  if (!existMenuItem) {
+    return next(new ErrorHandler("Menu Item Not Found", 404));
+  }
+
+  await Menu.findByIdAndDelete(id);
+
+  return res.status(200).json({
+    success: true,
+    message: "Menu Item deleted successfully",
+  });
+});
---
+++ src/routes/menu.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import { createItem, updateMenuItem, deleteMenuItem } from "../controllers/menu.controller.js";
+import { validateHandler, itemRegisterValidator } from "../lib/validator.js";
+import { upload } from "../middleware/multer.middleware.js";
+
+const router = Router();
+
+// Apply the verifyJWT middleware to all routes in this router
+router.use(verifyJWT);
+
+// Route to handle item creation
+router.route("/create").post(upload.single("image"),itemRegisterValidator(),validateHandler,createItem);
+
+router.route("/update/:id").put(updateMenuItem);
+
+router.route("/delete/:id").delete(deleteMenuItem)
+
+export default router;
