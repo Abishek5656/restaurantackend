4daeecfdf215720b4c4d4d9db46be3e4d05f2a29
Abishek Varma <abishek@entrolabs.com>
2024-12-31 12:37:04 +0530

    update app file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 
@@ -12,9 +14,16 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser())
+
+const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+const JWT_KEY  = process.env.JWT_SECREAT_KEY;
 
 //import Routes
+import userRouter from "./routes/admin.routes.js";
 
 //routes declaration
+app.use("/api/v1/admin", userRouter);
 
-export { app };
+
+export {envMode,JWT_KEY, app };
